f0b84cc0371f2deb49a0b709e303bb232824a2fb
arya631 <aryasreeragps@gmail.com>
2026-07-11 13:05:35 +0530

    navagation created

--- src/components/AddRegistrations.jsx
+++ src/components/AddRegistrations.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const AddRegistration = () => {
   return (
     <div>
+        <NavBar />
         <h2><center>Regisatration</center></h2>
         <div className="container">
             <div className="row">
+
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row">
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4">
--- src/components/SearchRegistration.jsx
+++ src/components/SearchRegistration.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const SearchRegistration = () => {
   return (
     <div>
-
+        <NavBar />
         <h2><center>Search Registration</center></h2>
         <div className="container">
 
